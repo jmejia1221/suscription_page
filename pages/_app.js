95438320d296580868d51a4911283283bad58d8a
John Mejia <jhondario.09@gmail.com>
2020-12-28 21:00:05 -0500

    Added some reset global styles and variables css

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 // This `pages/_app.js` file is useful to use styles.scss globally.
 
-import '../styles.module.scss';
+import '../styles.scss';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />

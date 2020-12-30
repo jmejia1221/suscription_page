4457f3b7a14d76f866781be22ab40651ea22339a
John Mejia <jhondario.09@gmail.com>
2020-12-29 23:50:19 -0500

    * Added users page * Added user components * styling user components

--- pages/users/index.jsx
+++ pages/users/index.jsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import { useRouter } from 'next/dist/client/router';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 
-const User = () => {
+// Components
+import UserList from '../../components/Users';
+import User from '../../components/Users/User';
+
+import * as action from '../../redux/actions';
+
+// CSS
+import styles from './users.module.scss';
+
+const Users = (props) => {
+    const router = useRouter();
+    useEffect(() => {
+        props.onGetUsers();
+    }, []);
+
+    const showUserDetail = (id) => {
+        router.push('/users/' + id);
+    }
+
+    let userListed = null;
+    if (props.users) {
+        userListed = props.users.map(user => {
+            return (
+                <User showUserDetail={showUserDetail} id={user.id} key={user.id} user={user.data} />
+            )
+        });
+    }
+
     return (
-        <h1>users</h1>
+        <div className={styles.content}>
+            <h1 className={styles.title}>users</h1>
+            <UserList>
+                {userListed}
+            </UserList>
+        </div>
     );
 };
 
-export default User;
+const mapStateToProps = state => ({
+    users: state.users.users
+});
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onGetUsers: () => dispatch(action.getUsers())
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Users);
